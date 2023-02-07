84bda76814ba024bed9a9adfec3dcd566c70e08f
LuanRDev <luan.dev14@gmail.com>
2023-02-07 17:30:29 -0400

    feat: Adicionando tratamento para exceção específica.

--- src/ApiEventos.WebApi/Program.cs
+++ src/ApiEventos.WebApi/Program.cs
@@ -1,4 +1,5 @@
 using ApiEventos.Application.DI;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,7 +23,7 @@ builder.Services.AddCors(o => o.AddPolicy("CorsPolicy", builder =>
            .AllowAnyHeader();
 }));
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
