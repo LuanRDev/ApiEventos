09ef8b6948001d390d3b567a6fadfac879bade49
Luan Rocha <luan.dev14@gmail.com>
2023-02-07 00:34:14 -0400

    feat: Adicionando propriedades e configurações para resolver os conflitos com o Entity Framework

--- src/ApiEventos.Domain/Models/StorageFile.cs
+++ src/ApiEventos.Domain/Models/StorageFile.cs
@@ -1,16 +1,25 @@
-﻿namespace ApiEventos.Domain.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiEventos.Domain.Models
 {
     public class StorageFile : BaseEntity
     {
-        public Guid GuidStorageId { get; private set; }
+        [NotMapped]
+        public Guid? GuidStorageId { get; private set; }
         public int CodigoEvento { get; set; }
-        public string Empresa { get; set; }
-        public string Name { get; set; }
-        public string Url { get; private set; }
-        public string Metadata { get; set; }
-        public string Type { get; set; }
-        public string Extension { get; set; }
-        public Byte[] Bytes { get; set; }
+        [NotMapped]
+        public string? Empresa { get; set; }
+        public string? Name { get; set; }
+        public string? Url { get; private set; }
+        [NotMapped]
+        public string? Metadata { get; set; }
+        [NotMapped]
+        public string? Type { get; set; }
+        [NotMapped]
+        public string? Extension { get; set; }
+        [NotMapped]
+        public Byte[]? Bytes { get; set; }
+        public StorageFile() { }
         public StorageFile(int codigoEvento, string empresa, string name, string metadata, string type, string extension, byte[] bytes)
         {
             GuidStorageId = Guid.NewGuid();
