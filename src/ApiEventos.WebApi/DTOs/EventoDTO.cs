4b23730454325dea78b21ac3755ffb82e667296d
LuanRDev <luan.dev14@gmail.com>
2023-02-08 17:17:31 -0400

    feat: Permitindo que as propriedades sejam anulaveis para contemplar o metodo update

--- src/ApiEventos.WebApi/DTOs/EventoDTO.cs
+++ src/ApiEventos.WebApi/DTOs/EventoDTO.cs
@@ -5,15 +5,15 @@ namespace ApiEventos.WebApi.DTOs
     public class EventoDTO
     {
         public int Id { get; set; }
-        public int TipoEvento { get; set; }
+        public int? TipoEvento { get; set; }
         public string? Descricao { get; set; }
         public string Empresa { get; set; }
         public string? Instrutor { get; set; }
-        public DateTime DataRealizado { get; set; }
-        public float CargaHoraria { get; set; }
-        public int ParticipantesEsperados { get; set; }
-        public int ParticipacoesConfirmadas { get; set; }
-        public bool Inativo { get; set; }
-        public IEnumerable<string> ArquivosBase64 { get; set; }
+        public DateTime? DataRealizado { get; set; }
+        public float? CargaHoraria { get; set; }
+        public int? ParticipantesEsperados { get; set; }
+        public int? ParticipacoesConfirmadas { get; set; }
+        public bool? Inativo { get; set; }
+        public IEnumerable<string>? ArquivosBase64 { get; set; }
     }
 }
