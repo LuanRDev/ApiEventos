33726fbd5e0fa35dd5b0661f79c5474ed52c564a
LuanRDev <luan.dev14@gmail.com>
2023-02-14 17:32:24 -0400

    feat: Adicionando o middleware de excecoes

---
+++ src/ApiEventos.WebApi/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+
+namespace ApiEventos.WebApi.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILoggerManager_logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILoggerManager logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpConext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError($"Algo deu errado: {ex}");
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+        private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            await context.Response.WriteAsync(new ErrorDetails()
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = "Internal Server Error from the custom middleware."
+            }.ToString());
+        }
+    }
+}
