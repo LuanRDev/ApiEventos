7b44500b2e14f4df4994de1ba3fedc666993d705
LuanRDev <luan.dev14@gmail.com>
2023-02-18 12:02:42 -0400

    build: Adicionando o Serilog na API

--- src/ApiEventos.WebApi/Program.cs
+++ src/ApiEventos.WebApi/Program.cs
@@ -1,6 +1,8 @@
 using ApiEventos.Application.DI;
 using ApiEventos.WebApi.Middlewares;
 using System.Text.Json.Serialization;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,11 +13,30 @@ builder.Host.ConfigureAppConfiguration(app => app.AddConfiguration(
         .AddUserSecrets<Program>()
         .AddEnvironmentVariables()
         .Build()
-    ));
-
+    )).ConfigureWebHostDefaults(webBuilder =>
+    {
+        webBuilder
+            .UseSerilog((ctx, cfg) =>
+            {
+                cfg.Enrich.FromLogContext()
+                    .Enrich.WithMachineName()
+                    .WriteTo.Console()
+                    .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(ctx.Configuration["ElasticConfiguration:Uri"]))
+                    {
+                        IndexFormat = $"{ctx.Configuration["ApplicationName"]}-logs-{ctx.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}",
+                        AutoRegisterTemplate = true,
+                        NumberOfShards = 2,
+                        NumberOfReplicas = 1
+                    })
+                   .Enrich.WithProperty("Environment", ctx.HostingEnvironment.EnvironmentName)
+                   .ReadFrom.Configuration(ctx.Configuration);
+            });
+    });
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+AppContext.SetSwitch("Npgsql.DisableDateTimeInfinityConversions", true);
 // Add services to the container.
 
-Initializer.Configure(builder.Services, builder.Configuration.GetConnectionString("DefaultConnectionString"));
+Initializer.Configure(builder.Services, builder.Configuration.GetConnectionString("DefaultConnectionString")!);
 
 builder.Services.AddCors(o => o.AddPolicy("CorsPolicy", builder =>
 {
@@ -38,7 +59,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseMiddleware<ExceptionMiddleware>();
-
 app.UseCors("CorsPolicy");
 
 app.UseHttpsRedirection();
